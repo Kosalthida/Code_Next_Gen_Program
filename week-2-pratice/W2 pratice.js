437aba05b155b47491c0cc2c2f1b15cc644a33b3
Kosalthida <damsie.sie@gamil.com>
2025-08-11 12:08:17 +0700

    done

--- week-2-pratice/W2 pratice.js
+++ week-2-pratice/W2 pratice.js
@@ -1,21 +1,3 @@
-// Exercise 1 Greeting Card
-// let name="Thida"
-// let message="Hello,"
-
-// console.log(message,name)
-
-//Ex2 Age Calculator
-// let currentYear=2025
-// let birthYear=2006
-// let age=currentYear-birthYear
-// console.log("You are",age,"year olds")
-
- //Ex 3 My favorite Food
-// let favoriteFood = ["Mi hel","Boklahong","Kimbap"]
-// for(let i=0;i<3;i++){
-//     console.log("I like to eat" ,favoriteFood[i])
-// }
-
 //Week 2 pratice :
 // Lab 1:No Return but Accepts Parameter
 
